42bc8f9c9294f761fb5a34f925a021e9243ff916
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-08 12:55:09 +0530

    Compiling the ANN

--- Business Problem - Neural Network( using Keras)/Business Problem - Neural Network( using Keras).py
+++ Business Problem - Neural Network( using Keras)/Business Problem - Neural Network( using Keras).py
@@ -98,6 +98,9 @@ classifier.add(Dense(units=6,kernel_initializer='uniform',activation='relu'))
 classifier.add(Dense(units=1,kernel_initializer='uniform',activation='sigmoid'))
 
 
+#Compiling the ANN
+
+classifier.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
 
 
