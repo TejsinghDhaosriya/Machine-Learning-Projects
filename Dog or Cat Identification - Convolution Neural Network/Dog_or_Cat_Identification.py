91788b8d56de17ce61acfba4bf86fee0cfe592e2
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-16 12:20:21 +0530

    Dog or Cat Project

---
+++ Dog or Cat Identification - Convolution Neural Network/Dog_or_Cat_Identification.py
@@ -0,0 +1,8 @@
+#Importing the Keras Libraries
+
+from keras.models import Sequential
+#form keras.layers import Convolution2D
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
